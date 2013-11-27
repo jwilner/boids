b09dfa6462afe67b7374a8363365aae580976a6f
Joe Wilner <jhwilner1@gmail.com>
2013-11-26 23:51:37 -0500

    Added obstacle avoidance, replaced swap! with reset!, general refactoring.

--- src/boids/core.cljs
+++ src/boids/core.cljs
@@ -5,7 +5,6 @@
   (:require-macros [cljs.core.async.macros :refer [go]]))
 
 (def canvas (.getElementById js/document "sky"))
-(def canvas-color "white")
 (def context (.getContext canvas "2d"))
 (def canvas-dimensions [(.-width canvas)
                         (.-height canvas)])
@@ -19,6 +18,9 @@
 (def inertia (atom default-inertia))
 (def boids (atom {}))
 (def goal (atom nil))
+(def obstacle (atom nil))
+(def obstacle-template {:xy [200 200] :radius 100})
+(def obstacle-timeout 10000)
 
 (defn print-func [& x]
   (. js/console (log (apply str x))))
@@ -38,6 +40,20 @@
   [context bird]
   (let [[x y] (:xy bird)]
     (.clearRect context x y 5 5)))
+
+(defn render-obstacle! 
+  [color]
+  (let [{xy :xy radius :radius} @obstacle]
+    (set! (.-strokeStyle context) color)
+    (.beginPath context)
+    (.arc context (first xy) (second xy) (- radius 20) 0 (* 2 Math/PI))
+    (.stroke context)))
+
+(defn draw-obstacle! []
+  (render-obstacle! "black"))
+
+(defn erase-obstacle! []
+  (render-obstacle! "white"))
 
 (defn wrap [v] (mapv mod v canvas-dimensions))
 
@@ -62,8 +78,7 @@
         rooted (Math/sqrt summed)]
     (mapv #(/ % rooted) v)))
 
-;; turn funcs and helpers
-
+;; turn func helpers
 (defn if-empty-wrapper
   [func neighbors bird]
   (if (empty? neighbors)
@@ -79,6 +94,19 @@
   [{:keys [xy] :as bird} dest]
   (direction xy dest))
 
+(defn birds-within-radius
+  "bird [list of birds :as flock] Number -> [list of birds :as sub-flock]."
+  [{:keys [xy] :as bird} flock radius]
+  (filter #(< (distance xy (:xy %)) radius) flock))
+
+(defn weighted-repulsion
+  "tuple tuple int -> heading."
+  [repulse-location location radius]
+  (let [repulse-weight (/ radius (distance location repulse-location))]
+    (mapv (partial * repulse-weight) 
+          (direction repulse-location location))))
+
+; turn-funcs
 (defn adhere-to-center
   "[list of birds] bird -> heading."
   [neighbors bird]
@@ -88,47 +116,47 @@
                     (mapv #(/ % (count neighbors))))]
     (heading-to-dest bird center)))
 
-;; TODO birds-within-radius infactor
-
-(defn birds-within-radius
-  "bird [list of birds :as flock] Number -> [list of birds :as sub-flock]."
-  [{:keys [xy] :as bird} flock radius]
-  (filter #(< (distance xy (:xy %)) radius) flock))
-
 (defn maintain-separation 
   "Flock, bird -> heading."
   [flock {:keys [xy] :as bird}]
   (let [birds-too-close (birds-within-radius bird flock min-separation)
-        weight (fn [b2]
-                 (/ min-separation (distance xy (:xy b2))))
-        get-away-dir (fn [bird] (direction (:xy bird) xy))
-        weighted-away-dir (fn [bird] (mapv #(* (weight bird) %) (get-away-dir bird)))
+        weighted-away-dir #(weighted-repulsion (:xy %) xy min-separation)
         away-dirs (map weighted-away-dir birds-too-close)
         result (apply sum-vectors away-dirs)]
     (if (empty? result) [0 0] result)))
 
 (defn align-direction 
+  "Flock, bird -> heading."
   [flock bird]
   (apply sum-vectors (map :heading flock)))
 
 (defn go-for-goal
+  "Flock (ignored), bird -> heading."
   [_ bird]
   (if-let [g @goal]
     (heading-to-dest bird g)
     [0 0]))
 
+(defn obstacle-avoidance
+  [_ bird]
+  "Flock (ignored), bird -> heading."
+  (if-let [{xy :xy radius :radius}  @obstacle]
+    (if (< (distance xy (:xy bird)) radius)
+      (mapv (partial * 100) (weighted-repulsion xy (:xy bird) radius))
+      [0 0])
+    [0 0]))
+
 ;; bird functions
+
 (defn update-heading
   "bird, [list of birds] -> bird with new heading."
   [{:keys [turn-funcs heading xy] :as bird} flock]
-  (let [flockers (remove (partial = bird) flock)
-        visible-birds (birds-within-radius bird flockers visible-range)
-        list-of-new-headings (map (fn [func]
-                                    (let [res (func visible-birds bird)]
-                                     (when (empty? res)
-                                      (print-func func))
-                                      res))
-                                       turn-funcs)
+  (let [visible-birds (remove (partial = bird) 
+                              (birds-within-radius bird 
+                                                   flock 
+                                                   visible-range))
+        list-of-new-headings (map #(% visible-birds bird)
+                                  turn-funcs)
         new-heading (normalize-vector (apply sum-vectors 
                                              (mapv (partial * @inertia) heading)
                                              (remove empty? list-of-new-headings)))]
@@ -170,16 +198,25 @@
     out))
 
 (defn handle-events []
-  (let [events (listen (dom/getElement "sky") "mousemove" "mouseout")]
+  (let [events (listen (dom/getElement "sky") "mousemove" "mouseout" "click")]
     (go
       (while true
         (let [e (<! events)
               t (.-type e)]
           (condp = t
-            "mousemove" (do (swap! goal (fn[] [(.-clientX e) (.-clientY e)]))
-                            (swap! inertia (fn[] (/ default-inertia 2))))
-            "mouseout" (do (swap! goal (fn[] nil))
-                           (swap! inertia (fn[] default-inertia)))))))))
+            "mousemove" (do (reset! goal [(.-clientX e) (.-clientY e)])
+                            (reset! inertia (/ default-inertia 2)))
+            "mouseout" (do (reset! goal nil)
+                           (reset! inertia default-inertia))
+            "click" (go (do 
+                          (let [xy [(.-clientX e) (.-clientY e)]]
+                            (when-let [o @obstacle]
+                              (erase-obstacle!))
+                            (reset! obstacle (assoc obstacle-template :xy xy))
+                            (draw-obstacle!)
+                            (<! (timeout obstacle-timeout))
+                            (erase-obstacle!)
+                            (reset! obstacle nil))))))))))
 
 (doseq [n (range num-birds)]
   (register-bird!
@@ -189,6 +226,7 @@
      :turn-funcs [(partial if-empty-wrapper adhere-to-center)
                   (partial if-empty-wrapper maintain-separation)
                   (partial if-empty-wrapper align-direction)
+                  obstacle-avoidance
                   go-for-goal]
      :uid n
      :speed 1000}))
