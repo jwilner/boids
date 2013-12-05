5e9bcddce89bf02355388d5ad3cb4534772f389f
Gideon Dresdner <gideond@gmail.com>
2013-12-05 14:25:54 -0500

    Implement ILookup instead of get methods.

--- src/boids/vector.cljs
+++ src/boids/vector.cljs
@@ -3,26 +3,22 @@
 (defn square [n] (Math/pow n 2))
 
 (defprotocol ISpatialVector
-  (as-vec [this] "Returns the SpatialVector as a Persistent Vector")
+  (as-vec [this] "SpatialVector -> Persistent Vector")
   (add [this other] "Adds two vectors (coordinate-wise).")
   (subt [this other] "Subtracts the 2nd from the 1st (coordinate-wise).")
   (distance [this other] "Euclidean distance between two SpatialVectors.")
   (length [this] "Distance to zero.")
-  (wrap [this modulus] "Mods the vector by modulus (coordinate-wise)."))
-
-(defprotocol IVector2d
-  (x [this])
-  (y [this]))
+  (normalize [this] "Divides a vector by its length, returning a vector of length = 1.")
+  (wrap [this modulus-x modulus-y] "Mods the vector by modulus (coordinate-wise)."))
 
 (deftype Vector2d
-  [v1 v2]
+  [x y]
   ISpatialVector
   (add [this other]
-    (Vector2d. (+ (x this) (x other)) (+ (y this) (y other))))
+    (Vector2d. (+ (:x this) (x other)) (+ (:y this) (:y other))))
   (subt [this other]
-    (Vector2d. (- (x this) (x other)) (- (y this ) (y other))))
-  (as-vec [this]
-    [v1 v2])
+    (Vector2d. (- (:x this) (:x other)) (- (:y this ) (:y other))))
+  (as-vec [this] [x y])
   (distance
     [this other]
     (Math/sqrt
@@ -30,11 +26,21 @@
          (square (- (nth this 1) (nth other 1))))))
   (length [this]
     (distance this (Vector2d. 0 0)))
-  (wrap [this modulus]
-    (Vector2d. (mod (x this) modulus) (mod (y this) modulus)))
-  IVector2d
-  (x [this] v1)
-  (y [this] v2))
+  (normalize [this] (let [l (length this)]
+      (Vector2d. (/ (:x this) l) (/ (:y this) l))))
+  (wrap [this modulus-x modulus-y]
+    (Vector2d. (mod (:x this) modulus-x) (mod (y this) modulus-y)))
+  ILookup
+  (-lookup [this key default]
+    (condp keyword-identical? key
+      :x x
+      :y y
+      default))
+  (-lookup [this key]
+    (condp keyword-identical? key
+      :x x
+      :y y
+      nil)))
 
 (defn print-func [& x]
   (. js/console (log (apply str x))))
